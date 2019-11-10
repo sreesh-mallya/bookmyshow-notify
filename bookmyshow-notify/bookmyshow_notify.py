e2759806cae110875f6646965ea5601ad9ee574a
Sreesh Mallya <sreeshsmallya@gmail.com>
2019-11-10 12:21:28 +0530

    Completed feature; Implemented logging

--- bookmyshow-notify/bookmyshow_notify.py
+++ bookmyshow-notify/bookmyshow_notify.py
@@ -5,10 +5,15 @@ import sched
 import time
 from urllib.request import urlopen, Request, urlretrieve
 import argparse
+import logging
 
 from bs4 import BeautifulSoup
 
 from utils import build_url, validate_date
+
+logger_handle = 'bookmyshow_notify'
+logging.basicConfig(level=logging.NOTSET, format='[%(asctime)s] [%(levelname)s] \t %(message)s', datefmt='%d-%m-%Y %H:%M:%S')
+logger = logging.getLogger(logger_handle)
 
 
 def parse_args(cl_args):
@@ -43,17 +48,26 @@ def check_for_keywords(venue_list, keywords):
 
 
 def keep_checking(schdlr, url, keywords):
+	logger.info('Checking for shows.')
 	venue_list = get_venue_list(url)
 	if check_for_keywords(venue_list, keywords):
-		webbrowser.open()
+		logger.info('Successfully found match.')
+		webbrowser.open(url)
+		exit()
 	else:
-		schdlr.enter(60, 1, keep_checking, (schdlr, ))
+		logger.info('Couldn\'t find any match. Trying again.')
+		schdlr.enter(60, 1, keep_checking, (schdlr, url, keywords))
 
 
 if __name__ == '__main__':
 	import sys
 	args = parse_args(sys.argv[1:])
+	logger.debug('Recieved arguments \nURL: {}\nDate: {}\nKeywords: {}\n'.format(args.url, args.date, args.keywords))
 	bookmyshow_url = build_url(args.url, args.date)
+	logger.debug('BookMyShow booking page URL: ' + bookmyshow_url)
 	schdlr = sched.scheduler(time.time, time.sleep)
-	schdlr.enter(60, 1, keep_checking, (schdlr, bookmyshow_url, keywords)) 
-	schdlr.run()
+	schdlr.enter(60, 1, keep_checking, (schdlr, bookmyshow_url, args.keywords)) 
+	try:
+		schdlr.run()
+	except KeyboardInterrupt:
+		logger.info("Exiting.")
